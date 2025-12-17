db3d6ae3b0a01e55367ecba7b3d10f8270ca2e01
akgit00 <tkfsecond@gmail.com>
2025-12-17 17:04:55 -0500

    create a class that holds all the data associated with a customer's order

--- src/main/java/org/yearup/models/Order.java
+++ src/main/java/org/yearup/models/Order.java
@@ -5,9 +5,16 @@ import java.time.LocalDate;
 
 public class Order {
 
+    //unique identifier for the order (primary key in the database)
     private Integer orderID;
+
+    //the ID of the user who placed the order
     private Integer userID;
+
+    //the date the order was created/placed
     private LocalDate date;
+
+    //shipping address information for delivering the order
     private String address;
     private String city;
     private String state;
@@ -30,3 +37,63 @@ public class Order {
     public Integer getOrderID() {
         return orderID;
     }
+    public void setOrderID(Integer orderID) {
+        this.orderID = orderID;
+    }
+
+
+    public Integer getUserID() {
+        return userID;
+    }
+    public void setUserID(Integer userID) {
+        this.userID = userID;
+    }
+
+
+    public LocalDate getDate() {
+        return date;
+    }
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+
+    public String getState() {
+        return state;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
+
+
+    public String getZip() {
+        return zip;
+    }
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+}
